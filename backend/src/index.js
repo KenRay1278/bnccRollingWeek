86cd1a812713f5793987c0c78bc78ead7719b94c
Kenneth Raymond <vkenneth.raymond@gmail.com>
2025-11-27 01:05:08 +0700

    Added FrontEnd by Michael Valentinus Rustam

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import feedbackRoutes from './routes/feedbackRoutes.js';
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 app.use(express.json());
 app.use('/api/feedback',feedbackRoutes);
 
